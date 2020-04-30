5f3ce5a0d6e7fa6d960621959bf78bcc8be320f2
Andrea Ramirez <anlramirezs11@gmail.com>
2020-04-30 17:00:24 -0500

    Se crea la tabla para presentar la suma total de medallas

--- src/app/components/resumen/resumen.component.ts
+++ src/app/components/resumen/resumen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OlimpicWinnersService } from '../../services/olimpic-winners.service';
 
 @Component({
   selector: 'app-resumen',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resumen.component.css']
 })
 export class ResumenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  winners: any;
+  medallas: any;
+  constructor(private olimpicWinnersService: OlimpicWinnersService) { 
+    this.medallas = [{
+      "tipoMedalla": "Oro",
+      "acumulado":0,
+      "Image": "../../../assets/image/oro.jpg"
+    },
+    {
+      "tipoMedalla": "Plata",
+      "acumulado":0,
+      "Image": "../../../assets/image/plata.jpg"
+    },
+    {
+      "tipoMedalla": "Bronce",
+      "acumulado":0,
+      "Image": "../../../assets/image/bronce.jpg"
+    },
+    {
+      "tipoMedalla": "Total",
+      "acumulado":0,
+      "Image": "../../../assets/image/todas.jpg"
+    }
+  ]
   }
 
+  ngOnInit() {
+    this.olimpicWinnersService.getData().subscribe(winners => {
+      this.winners = winners;
+      this.winners.forEach(winner => {
+        this.medallas[0].acumulado += winner.gold;
+        this.medallas[1].acumulado += winner.silver;
+        this.medallas[2].acumulado += winner.bronze;
+        this.medallas[3].acumulado += winner.total;
+      });
+      console.log(this.medallas)
+
+    }, (errorService) => {
+      console.log(errorService);
+    });
+  }
 }
