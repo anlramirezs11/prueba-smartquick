9e70871b7903bf055bcf3ae20fdbed1be43bc6e8
Andrea Ramirez <anlramirezs11@gmail.com>
2020-04-30 10:16:24 -0500

    Se implementa el login del app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AdministradorComponent } from './pages/administrador/administrador.component';
 import { CoodinadorComponent } from './pages/coodinador/coodinador.component';
@@ -10,7 +9,6 @@ import { CoodinadorComponent } from './pages/coodinador/coodinador.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent,
     LoginComponent,
     AdministradorComponent,
     CoodinadorComponent
